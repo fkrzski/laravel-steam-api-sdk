8c8af84654a07378ab65671cbb60bcf963959233
Filip Krzyżanowski <filip.krzyzanowski@protonmail.com>
2026-06-13 16:06:39 +0200

    chore: update Steam facade to use fully qualified class names

--- src/Facades/Steam.php
+++ src/Facades/Steam.php
@@ -5,28 +5,18 @@ declare(strict_types=1);
 namespace Fkrzski\LaravelSteamApiSdk\Facades;
 
 use Fkrzski\LaravelSteamApiSdk\SteamManager;
-use Fkrzski\SteamApiSdk\Dto\OwnedGame;
-use Fkrzski\SteamApiSdk\Dto\PlayerAchievements;
-use Fkrzski\SteamApiSdk\Dto\PlayerSummary;
-use Fkrzski\SteamApiSdk\Dto\UserStats;
-use Fkrzski\SteamApiSdk\SteamConnector;
-use Fkrzski\SteamApiSdk\ValueObjects\SteamId;
 use Illuminate\Support\Facades\Facade;
-use Saloon\Http\Faking\MockClient;
-use Saloon\Http\Pool;
-use Saloon\Http\Request;
-use Saloon\Http\Response;
 
 /**
- * @method static SteamConnector connector()
- * @method static Response send(Request $request)
- * @method static Pool pool(iterable<Request>|callable $requests = [], int|callable $concurrency = 5, ?callable $responseHandler = null, ?callable $exceptionHandler = null)
- * @method static list<PlayerSummary> playerSummaries(list<SteamId> $steamIds)
- * @method static list<OwnedGame> ownedGames(SteamId $steamId, list<int> $appIdsFilter = [], bool $includeAppInfo = false, bool $includePlayedFreeGames = false)
- * @method static UserStats userStatsForGame(SteamId $steamId, int $appId, ?string $language = null)
- * @method static PlayerAchievements playerAchievements(SteamId $steamId, int $appId, ?string $language = null)
- * @method static SteamId resolveVanityUrl(string $vanityName)
- * @method static MockClient fake(array<class-string, mixed> $responses = [])
+ * @method static \Fkrzski\SteamApiSdk\SteamConnector connector()
+ * @method static \Saloon\Http\Response send(\Saloon\Http\Request $request)
+ * @method static \Saloon\Http\Pool pool(iterable<\Saloon\Http\Request>|callable $requests = [], int|callable $concurrency = 5, ?callable $responseHandler = null, ?callable $exceptionHandler = null)
+ * @method static list<\Fkrzski\SteamApiSdk\Dto\PlayerSummary> playerSummaries(list<\Fkrzski\SteamApiSdk\ValueObjects\SteamId> $steamIds)
+ * @method static list<\Fkrzski\SteamApiSdk\Dto\OwnedGame> ownedGames(\Fkrzski\SteamApiSdk\ValueObjects\SteamId $steamId, list<int> $appIdsFilter = [], bool $includeAppInfo = false, bool $includePlayedFreeGames = false)
+ * @method static \Fkrzski\SteamApiSdk\Dto\UserStats userStatsForGame(\Fkrzski\SteamApiSdk\ValueObjects\SteamId $steamId, int $appId, ?string $language = null)
+ * @method static \Fkrzski\SteamApiSdk\Dto\PlayerAchievements playerAchievements(\Fkrzski\SteamApiSdk\ValueObjects\SteamId $steamId, int $appId, ?string $language = null)
+ * @method static \Fkrzski\SteamApiSdk\ValueObjects\SteamId resolveVanityUrl(string $vanityName)
+ * @method static \Saloon\Http\Faking\MockClient fake(array<class-string, mixed> $responses = [])
  *
  * @see SteamManager
  */
